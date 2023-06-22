7dfe88f81bbc9cc1431096887776f43d66fe29c9
Prerana <prerana@hellosivi.com>
2023-06-22 06:20:42 +0530

    data transform, stats and dummy model added

--- data.py
+++ data.py
@@ -2,15 +2,20 @@ import torch
 from torchvision import datasets, transforms
 
 
-def get_transforms():
-    transform = transforms.Compose([
+def get_transforms(normalize):
+    if not normalize:
+        transform = transforms.Compose([
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.5,), (0.25,))
-    ])
+                        ])
+    else:
+        transform = transforms.Compose([
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.49, 0.45), (0.23, 0.23, 0.25))
+        ])
     return transform
     
-def get_data(train=False):
-    transforms = get_transforms()
+def get_data(train=False, normalize=False):
+    transforms = get_transforms(normalize)
     print(transforms)
     if train:
         data = datasets.CIFAR10('./data', train=True, download=True, transform=transforms)
---
+++ utils_plot.py
@@ -0,0 +1,11 @@
+# functions to show an image
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
